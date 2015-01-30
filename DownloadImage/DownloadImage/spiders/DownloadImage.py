296b3059e73f7d0437c9c79b556a4fcffc8cb1b5
frank1994 <hejun1874@163.com>
2015-01-30 16:44:16 +0800

    first commit

---
+++ DownloadImage/DownloadImage/spiders/DownloadImage.py
@@ -0,0 +1,49 @@
+# coding:utf-8
+
+# ###################################################################
+# Description:      输入网址后自动只看楼主并保存到本地文件
+#                       将楼主发布的页面下载并存储到本地文件夹。
+# Author:           Frank
+# Date:             2015.01.28
+# Version:          0.1
+# ###################################################################
+
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from scrapy.http import Request
+import os
+
+class GetUrlSpider(BaseSpider):
+    name = "downloadspider"
+    allowed_domains = ["douban.com"]
+    download_delay = 0.2
+    counter = 0;
+    if not os.path.exists('./Image'):
+        os.mkdir('./Image')
+
+    albumId = [[139011725,13]]
+    start_urls = [];
+    for [album,page] in albumId:
+    	for i in range(0,18*(page+1),18):
+                start_urls.append('http://douban.com/photos/album/%d/?start=%d' % (album,i))
+
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+        req = []
+        # sites = hxs.select("//ul/li/div[@class='cover']/a/img/@src").extract()
+        sites = hxs.select("//div[@class='photo_wrap']/a/img/@src").extract()
+        for site in sites:
+            print site
+            site = site.replace('thumb','photo')
+            r = Request(site, callback=self.DownLload)
+            req.append(r)
+        return req
+
+    def DownLload(self, response):
+    	str = response.url[0:-3];
+        self.counter = self.counter+1
+        str = str.split('/');
+        print '----------------Image Get----------------',self.counter,str[-1],'jpg'
+        imgfile = open('./Image/'+str[-1]+"jpg",'wb')
+        imgfile.write(response.body)
+        imgfile.close()
---
+++ README.md
@@ -0,0 +1,32 @@
+豆瓣相册爬虫
+===================
+
+
+###运行环境：
+
+* python版本为2.7x
+* scrapy
+
+###使用方法：
+
+####1.设置要下载的页面id和页数:
+修改`...\douban-image-scrapy\DownloadImage\DownloadImage\spiders`的`DownloadImage.py`,找到`albumId = [....]`
+第一个值是相册的id，第二个值是相册的页数。例如：
+
+```
+albumId = [[139011725,13]]
+```
+修改后保存即可！
+
+####2.运行
+
+```
+cd .\DownloadImage
+scrapy crawl downloadspider
+```
+
+运行完毕后，图片保存在`...\douban-image-scrapy\DownloadImage\Image`
+
+###后期改进：
+* 自动检测相册页数，只要输入id即可
+* 按相册分类建文件夹存放图片
